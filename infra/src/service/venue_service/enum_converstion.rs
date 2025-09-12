7b2177df4126c85be5536577bac7800ec6ce6b24
lexon <lexonnewb@foxmail.com>
2025-09-12 15:13:20 +0800

    chore(infra):Rename venue conversation fn name.

--- infra/src/service/venue_service/enum_converstion.rs
+++ infra/src/service/venue_service/enum_converstion.rs
@@ -2,14 +2,14 @@ use domain_core::venue::VenueStatus as CoreVenueStatus;
 
 use crate::database::entities::sea_orm_active_enums::VenueState;
 
-pub(crate) fn user_status_to_db(value: CoreVenueStatus) -> VenueState {
+pub(crate) fn venue_status_to_db(value: CoreVenueStatus) -> VenueState {
     match value {
         CoreVenueStatus::Published => VenueState::Published,
         CoreVenueStatus::Unpublished => VenueState::Unpublished,
     }
 }
 
-pub(crate) fn user_status_to_domain(value: VenueState) -> CoreVenueStatus {
+pub(crate) fn venue_status_to_domain(value: VenueState) -> CoreVenueStatus {
     match value {
         VenueState::Published => CoreVenueStatus::Published,
         VenueState::Unpublished => CoreVenueStatus::Unpublished,
