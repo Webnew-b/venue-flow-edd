92708320b44e58e23dd0ae6c703ef69bf491c14a
lexon <lexonnewb@foxmail.com>
2025-12-12 15:33:58 +0800

    chore(infra):open log feat

--- infra/src/local_log/mod.rs
+++ infra/src/local_log/mod.rs
@@ -32,6 +32,8 @@ pub fn init_logger() -> Result<(), InfraError> {
         super::config::AppEnv::DEVELOP => {
             tracing_subscriber::fmt()
                 .with_env_filter(EnvFilter::from_default_env())
+                .with_file(true)
+                .with_line_number(true)
                 .init();
             println!("Develop logger loaded successed!");
             Ok(())
