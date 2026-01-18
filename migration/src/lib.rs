67fbc659f1dcbab7a6bf5f5f4ff0b8e7c1ec1a71
lexon <lexonnewb@foxmail.com>
2026-01-18 14:54:52 +0800

    fix(infra):remove  bigserial for all foregin key

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -32,6 +32,7 @@ mod m20250903_062328_fk_venue_to_venue_image_uri;
 mod m20250906_062421_remove_images_column_from_venue;
 mod m20250906_063933_change_allow_activity_to_enum;
 mod m20250906_065650_add_create_time_to_venue_image;
+mod m20260118_063958_remove_bigserial_for_all_fk;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
@@ -51,9 +52,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20250815_114354_create_organizer_table::Migration),
             Box::new(m20250815_114355_fk_venue_to_rental_request::Migration),
             Box::new(m20250815_114354_create_rental_request_table::Migration),
-            Box::new(
-                m20250815_114355_fk_organizer_to_rental_request::Migration,
-            ),
+            Box::new(m20250815_114355_fk_organizer_to_rental_request::Migration),
             Box::new(m20250823_152948_modify_user_table::Migration),
             Box::new(m20250824_064038_modify_requsest_status::Migration),
             Box::new(m20250825_064838_modify_user_table::Migration),
@@ -61,13 +60,10 @@ impl MigratorTrait for Migrator {
             Box::new(m20250829_062807_modify_rental_request::Migration),
             Box::new(m20250903_062327_create_venue_image_uri_table::Migration),
             Box::new(m20250903_062328_fk_venue_to_venue_image_uri::Migration),
-            Box::new(
-                m20250906_062421_remove_images_column_from_venue::Migration,
-            ),
+            Box::new(m20250906_062421_remove_images_column_from_venue::Migration),
             Box::new(m20250906_063933_change_allow_activity_to_enum::Migration),
-            Box::new(
-                m20250906_065650_add_create_time_to_venue_image::Migration,
-            ),
+            Box::new(m20250906_065650_add_create_time_to_venue_image::Migration),
+            Box::new(m20260118_063958_remove_bigserial_for_all_fk::Migration),
         ]
     }
 }
