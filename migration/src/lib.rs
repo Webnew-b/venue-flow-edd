f5af11cc827d5fdc624cb0881bdb4da3cbdf84e4
lexon <lexonnewb@foxmail.com>
2026-02-13 15:27:01 +0800

    fix(migration):Adjust the migration sequence.

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -44,15 +44,17 @@ impl MigratorTrait for Migrator {
             Box::new(m20250815_114353_user_status::Migration),
             Box::new(m20250815_114353_venue_state::Migration),
             Box::new(m20250815_114354_create_user_table::Migration),
-            Box::new(m20250815_114355_fk_user_to_lessor::Migration),
             Box::new(m20250815_114354_create_venue_table::Migration),
-            Box::new(m20250815_114355_fk_lessor_to_venue::Migration),
             Box::new(m20250815_114354_create_lessor_table::Migration),
-            Box::new(m20250815_114355_fk_user_to_organizer::Migration),
             Box::new(m20250815_114354_create_organizer_table::Migration),
-            Box::new(m20250815_114355_fk_venue_to_rental_request::Migration),
             Box::new(m20250815_114354_create_rental_request_table::Migration),
-            Box::new(m20250815_114355_fk_organizer_to_rental_request::Migration),
+            Box::new(m20250815_114355_fk_user_to_lessor::Migration),
+            Box::new(m20250815_114355_fk_lessor_to_venue::Migration),
+            Box::new(m20250815_114355_fk_user_to_organizer::Migration),
+            Box::new(m20250815_114355_fk_venue_to_rental_request::Migration),
+            Box::new(
+                m20250815_114355_fk_organizer_to_rental_request::Migration,
+            ),
             Box::new(m20250823_152948_modify_user_table::Migration),
             Box::new(m20250824_064038_modify_requsest_status::Migration),
             Box::new(m20250825_064838_modify_user_table::Migration),
@@ -60,9 +62,13 @@ impl MigratorTrait for Migrator {
             Box::new(m20250829_062807_modify_rental_request::Migration),
             Box::new(m20250903_062327_create_venue_image_uri_table::Migration),
             Box::new(m20250903_062328_fk_venue_to_venue_image_uri::Migration),
-            Box::new(m20250906_062421_remove_images_column_from_venue::Migration),
+            Box::new(
+                m20250906_062421_remove_images_column_from_venue::Migration,
+            ),
             Box::new(m20250906_063933_change_allow_activity_to_enum::Migration),
-            Box::new(m20250906_065650_add_create_time_to_venue_image::Migration),
+            Box::new(
+                m20250906_065650_add_create_time_to_venue_image::Migration,
+            ),
             Box::new(m20260118_063958_remove_bigserial_for_all_fk::Migration),
         ]
     }
